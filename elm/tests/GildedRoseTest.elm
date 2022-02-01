08d6214fdb368bba25195dea131275f407b1970f
Arne Goeteyn <arne@moonchase.be>
2022-02-01 18:21:56 +0100

    fix unit test

--- elm/tests/GildedRoseTest.elm
+++ elm/tests/GildedRoseTest.elm
@@ -1,10 +1,9 @@
-module GildedRoseTest exposing (..)
+module GildedRoseTest exposing (suite, testEmptyList)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import GildedRose exposing (..)
 import Test exposing (..)
-
 
 suite : Test
 suite =
@@ -14,5 +13,10 @@ suite =
                 foo =
                     Item "foo" 10 30
             in
-            Expect.equal foo.name "fixme"
+            Expect.equal foo.name "foo"
         )
+
+testEmptyList : Test
+testEmptyList =
+    test "test empty list" <|
+        \_ -> GildedRose.update_quality [] |> Expect.equal []
