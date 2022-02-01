ca631a11ce0c7bf8edc53c240f68b920e99db441
Arne Goeteyn <arne@moonchase.be>
2022-02-01 18:34:56 +0100

    extract item update to seperate function that checks types

--- elm/src/GildedRose.elm
+++ elm/src/GildedRose.elm
@@ -5,52 +5,80 @@ type alias Item =
     { name : String
     , sell_by : Int
     , quality : Int
-    , type_: ItemType
+    , type_ : ItemType
     }
 
-type ItemType = Legendary | Brie | Ticket | Regular
+
+type ItemType
+    = Legendary
+    | Brie
+    | Ticket
+    | Regular
 
 
-update_quality : List Item -> List Item
-update_quality items =
-    List.map
-        (\item ->
-            if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert" then
-                if item.quality < 50 then
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert" then
-                        if item.sell_by < 0 then
-                            { item | sell_by = item.sell_by - 1, quality = 0 }
+updateItem : Item -> Item
+updateItem item =
+    case item.type_ of
+        Legendary ->
+            item
 
-                        else if item.sell_by < 6 then
-                            { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
+        Brie ->
+            if item.quality < 50 then
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" then
+                    if item.sell_by < 0 then
+                        { item | sell_by = item.sell_by - 1, quality = 0 }
 
-                        else if item.sell_by < 11 then
-                            { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
+                    else if item.sell_by < 6 then
+                        { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
 
-                        else
-                            { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+                    else if item.sell_by < 11 then
+                        { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
 
                     else
                         { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
 
                 else
-                    { item | sell_by = item.sell_by }
+                    { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
 
-            else if item.name /= "Aged Brie" && item.name /= "Sulfuras, Hand of Ragnaros" then
-                if item.sell_by < 0 && item.quality > 0 then
-                    if item.quality >= 2 then
-                        { item | sell_by = item.sell_by - 1, quality = item.quality - 2 }
+            else
+                { item | sell_by = item.sell_by }
 
-                    else
+        Ticket ->
+            if item.quality < 50 then
+                if item.name == "Backstage passes to a TAFKAL80ETC concert" then
+                    if item.sell_by < 0 then
                         { item | sell_by = item.sell_by - 1, quality = 0 }
 
-                else if item.quality >= 1 then
-                    { item | sell_by = item.sell_by - 1, quality = item.quality - 1 }
+                    else if item.sell_by < 6 then
+                        { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
+
+                    else if item.sell_by < 11 then
+                        { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
+
+                    else
+                        { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+
+                else
+                    { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+
+            else
+                { item | sell_by = item.sell_by }
+
+        Regular ->
+            if item.sell_by < 0 && item.quality > 0 then
+                if item.quality >= 2 then
+                    { item | sell_by = item.sell_by - 1, quality = item.quality - 2 }
 
                 else
                     { item | sell_by = item.sell_by - 1, quality = 0 }
 
+            else if item.quality >= 1 then
+                { item | sell_by = item.sell_by - 1, quality = item.quality - 1 }
+
             else
-                item
-        )
-        items
+                { item | sell_by = item.sell_by - 1, quality = 0 }
+
+
+update_quality : List Item -> List Item
+update_quality =
+    List.map updateItem
