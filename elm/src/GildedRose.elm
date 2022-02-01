5f13f0322fd7ecb70ffd378905b201447d418e98
Arne Goeteyn <arne@moonchase.be>
2022-02-01 18:49:39 +0100

    add random tests comparing old function to new

--- elm/src/GildedRose.elm
+++ elm/src/GildedRose.elm
@@ -1,4 +1,4 @@
-module GildedRose exposing (Item, ItemType(..), update_quality)
+module GildedRose exposing (Item, ItemType(..), update_quality, update_quality_old)
 
 
 type alias Item =
@@ -64,3 +64,48 @@ updateItem item =
 update_quality : List Item -> List Item
 update_quality =
     List.map updateItem
+
+
+update_quality_old : List Item -> List Item
+update_quality_old items =
+    List.map
+        (\item ->
+            if item.name == "Aged Brie" || item.name == "Backstage passes to a TAFKAL80ETC concert" then
+                if item.quality < 50 then
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert" then
+                        if item.sell_by < 0 then
+                            { item | sell_by = item.sell_by - 1, quality = 0 }
+
+                        else if item.sell_by < 6 then
+                            { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
+
+                        else if item.sell_by < 11 then
+                            { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
+
+                        else
+                            { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+
+                    else
+                        { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+
+                else
+                    { item | sell_by = item.sell_by }
+
+            else if item.name /= "Aged Brie" && item.name /= "Sulfuras, Hand of Ragnaros" then
+                if item.sell_by < 0 && item.quality > 0 then
+                    if item.quality >= 2 then
+                        { item | sell_by = item.sell_by - 1, quality = item.quality - 2 }
+
+                    else
+                        { item | sell_by = item.sell_by - 1, quality = 0 }
+
+                else if item.quality >= 1 then
+                    { item | sell_by = item.sell_by - 1, quality = item.quality - 1 }
+
+                else
+                    { item | sell_by = item.sell_by - 1, quality = 0 }
+
+            else
+                item
+        )
+        items
--- elm/tests/GildedRoseTest.elm
+++ elm/tests/GildedRoseTest.elm
@@ -1,9 +1,49 @@
-module GildedRoseTest exposing (suite, testEmptyList, testLegendaryItem)
+module GildedRoseTest exposing (suite, testEmptyList, testLegendaryItem, testRandomList)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import GildedRose exposing (..)
 import Test exposing (..)
+import Array exposing (Array)
+    
+tupleFuzzer : Fuzzer Int -> Fuzzer Int -> Fuzzer (Int, Int)
+tupleFuzzer sell_by_fuzzer quality_fuzzer =
+        Fuzz.tuple (sell_by_fuzzer, quality_fuzzer)    
+
+typeFromName: String -> ItemType
+typeFromName name =
+    case name of
+        "Sulfuras, Hand of Ragnaros" -> Legendary
+        "Backstage passes to a TAFKAL80ETC concert" -> Ticket
+        "Aged Brie" -> Brie
+        _ -> Regular
+
+
+generateRandomItems : List (Int, Int) ->  List GildedRose.Item
+generateRandomItems valuesList =
+    let
+        names =
+            [ "Sulfuras, Hand of Ragnaros"
+            , "Apple Pie"
+            , "Elixer of the Mongoose"
+            , "Aged Brie"
+            , "French fries with extra mayo"
+            , "Backstage passes to a TAFKAL80ETC concert"
+            ] |> Array.fromList
+        nameIndex  = \i -> modBy (Array.length names) i |> \index -> Array.get index names |> Maybe.withDefault "An item representing an error"
+    in
+    List.map 
+        (\(sellBy, quality) ->
+            let
+                 name = nameIndex (sellBy + quality)
+                 type_ = typeFromName name
+            in
+            case type_ of
+                Legendary ->    legendaryItem sellBy
+                _ ->
+                    {sell_by = sellBy, quality = quality, name = nameIndex (sellBy + quality), type_ = typeFromName <| nameIndex (sellBy + quality)}) 
+        valuesList
+
 
 applyNTimes: Int -> (a -> a) -> a -> a
 applyNTimes n f value = 
@@ -32,8 +72,16 @@ testEmptyList =
 
 testLegendaryItem: Test
 testLegendaryItem =
-    
     fuzz2 int (Fuzz.intRange 0 5) "legendary item should always keep the same quality and sellBy" <|
         \sellBy days -> 
             Expect.equal [legendaryItem sellBy] (applyNTimes days GildedRose.update_quality [legendaryItem sellBy]) 
     
+testRandomList: Test
+testRandomList =
+    fuzz (list <| tupleFuzzer int (Fuzz.intRange 0 50)) "A random list should return the same result on the old function and the new function" <|
+        \t -> 
+            let
+                randomItems = generateRandomItems t  |> Debug.log "reao"
+            in
+            Expect.equal (GildedRose.update_quality randomItems) (GildedRose.update_quality_old randomItems)
+ 
