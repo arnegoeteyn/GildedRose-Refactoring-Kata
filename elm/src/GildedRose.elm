d77197ac29fb19497cc21dce80f090cbc41fd649
Arne Goeteyn <arne@moonchase.be>
2022-02-01 19:22:12 +0100

    extract all sellBy statements

--- elm/src/GildedRose.elm
+++ elm/src/GildedRose.elm
@@ -1,4 +1,5 @@
 module GildedRose exposing (Item, ItemType(..), update_quality, update_quality_old, typeFromName)
+import Html.Attributes exposing (type_)
 
 
 type alias Item =
@@ -22,57 +23,62 @@ typeFromName name =
         "Aged Brie" -> Brie
         _ -> Regular
 
-updateItem : Item -> Item
+updateSellBy: (Item, Bool) -> Item
+updateSellBy (item, shouldLower) = 
+    {item | sell_by = if shouldLower then item.sell_by - 1 else item.sell_by}
+
+updateItem : Item -> (Item, Bool)
 updateItem item =
     let
         type_ = typeFromName item.name
     in
     case type_ of
         Legendary ->
-            item
+            (item, False)
 
         Brie ->
             if item.quality < 50 then
-                { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+                ({ item | quality = item.quality + 1 }, True)
 
             else
-                { item | sell_by = item.sell_by }
+                (item, False)
 
         Ticket ->
             if item.quality < 50 then
-                if item.sell_by < 0 then
-                    { item | sell_by = item.sell_by - 1, quality = 0 }
+                (if item.sell_by < 0 then
+                    { item | quality = 0 }
 
                 else if item.sell_by < 6 then
-                    { item | sell_by = item.sell_by - 1, quality = item.quality + 3 }
+                    { item | quality = item.quality + 3 }
 
                 else if item.sell_by < 11 then
-                    { item | sell_by = item.sell_by - 1, quality = item.quality + 2 }
+                    { item | quality = item.quality + 2 }
 
                 else
-                    { item | sell_by = item.sell_by - 1, quality = item.quality + 1 }
+                    { item | quality = item.quality + 1 } 
+                , True)
 
             else
-                { item | sell_by = item.sell_by }
+                (item, False)
 
         Regular ->
             if item.sell_by < 0 && item.quality > 0 then
                 if item.quality >= 2 then
-                    { item | sell_by = item.sell_by - 1, quality = item.quality - 2 }
+                    ({ item | quality = item.quality - 2 }, True)
 
                 else
-                    { item | sell_by = item.sell_by - 1, quality = 0 }
+                    ({ item | quality = 0 }, True)
 
             else if item.quality >= 1 then
-                { item | sell_by = item.sell_by - 1, quality = item.quality - 1 }
+                ({ item | quality = item.quality - 1 }, True)
 
             else
-                { item | sell_by = item.sell_by - 1, quality = 0 }
+                ({ item | quality = 0 }, True)
 
 
 update_quality : List Item -> List Item
 update_quality =
-    List.map updateItem
+    List.map (updateItem >> updateSellBy)
 
 
 update_quality_old : List Item -> List Item
